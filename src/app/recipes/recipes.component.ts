9c8384b2e3cdd5746bb6e2439b8276483be85e0d
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-01 10:38:21 -0500

    Imported and added recipe-detail component to recipes.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RecipeItemComponent } from './recipe-list/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @Component({
   selector: 'rb-recipes',
