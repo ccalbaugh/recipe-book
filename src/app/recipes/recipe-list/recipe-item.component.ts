86c85a08ff6398da86891c99d6bed8b4535c0b04
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-01 09:44:12 -0500

    Imported Input to recipe-item, added markup to recipe-list.

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe'
 
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe'
   templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent implements OnInit {
-  recipe: Recipe;
+  @Input() recipe: Recipe;
   recipeId: number;
 
   constructor() { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
 
   constructor() { }
 
