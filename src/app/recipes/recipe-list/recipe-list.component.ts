8d9fa670ee7cc14890ca726b05fa5d9b0882cb26
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-01 10:02:02 -0500

    Created, Imported, and Bound new recipe dummy item to recipe-list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe';
+import { RecipeItemComponent } from './recipe-item.component';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -8,6 +9,7 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://centuryshop.cachefly.net/getDynamicImage.aspx?path=BOBXLK-MAIN.jpg');
 
   constructor() { }
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeItemComponent } from './recipe-list/recipe-item.component';
+
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html'
