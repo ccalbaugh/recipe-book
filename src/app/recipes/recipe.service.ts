1fb449a79159cb8b3f8651e384dd014fa4dae819
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-03 13:11:20 -0500

    Add ingredients to recipes array.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('avocado salsa chicken recipe', 'Dreamy, cheesy Avocado Salsa Chicken just might be your new favorite 30-minute dinner recipe!', 'http://wearychef.com/wp-content/uploads/2013/05/avocado-salsa-chicken-12.jpg', []),
+    new Recipe('avocado salsa chicken recipe', 'Dreamy, cheesy Avocado Salsa Chicken just might be your new favorite 30-minute dinner recipe!', 'http://wearychef.com/wp-content/uploads/2013/05/avocado-salsa-chicken-12.jpg', [
+      new Ingredient('Chicken Breast', 2),
+      new Ingredient('avocados', 2),
+      new Ingredient('Salsa', 1) 
+    ]),
     new Recipe('turkey sandwich with goat cheese and jam', ' This grown up turkey sandwich will make your tastebuds sing! ', 'http://wearychef.com/wp-content/uploads/2016/01/turkey-sandwich-with-goat-cheese-and-jam-17-750x500.jpg', [])
   ]
 
