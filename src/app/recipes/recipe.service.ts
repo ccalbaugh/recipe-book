60fe373b24b60bda168aa4ec5b3c23d553c5ee94
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-04 11:30:46 -0500

    Change picture and description for recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,12 +6,12 @@ import { Ingredient } from '../shared/ingredient';
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('avocado salsa chicken recipe', 'Dreamy, cheesy Avocado Salsa Chicken just might be your new favorite 30-minute dinner recipe!', 'http://wearychef.com/wp-content/uploads/2013/05/avocado-salsa-chicken-12.jpg', [
+    new Recipe('avocado salsa chicken recipe', 'Your new favorite 30-minute dinner recipe!', 'http://wearychef.com/wp-content/uploads/2013/05/avocado-salsa-chicken-12-150x150.jpg', [
       new Ingredient('Chicken Breast', 2),
       new Ingredient('avocados', 2),
       new Ingredient('Salsa', 1) 
     ]),
-    new Recipe('turkey sandwich with goat cheese and jam', ' This grown up turkey sandwich will make your tastebuds sing! ', 'http://wearychef.com/wp-content/uploads/2016/01/turkey-sandwich-with-goat-cheese-and-jam-17-750x500.jpg', [])
+    new Recipe('turkey sandwich with goat cheese and jam', 'Make your tastebuds sing! ', 'http://wearychef.com/wp-content/uploads/2016/01/turkey-sandwich-with-goat-cheese-and-jam-17-150x150.jpg', [])
   ]
 
   constructor() {}
