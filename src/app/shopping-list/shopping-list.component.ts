2d291bf4c1a4a15412818d779a797a1689f303f2
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-07 09:19:56 -0600

    Create method and property for selecting item.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,11 +9,16 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   items: Ingredient[] = [];
+  selectedItem: Ingredient = null; // Making sure there is no default selectedItem
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     this.items = this.sls.getItems();
+  }
+
+  onSelectItem(item: Ingredient) {
+    this.selectedItem = item; // this makes it so you can bind the item to the shopping-list-add component in the html.
   }
 
 }
