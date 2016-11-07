7bd23a31096641a0c299bf428f3ede5d79877fa3
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-07 16:28:39 -0600

    Add return to fix bug in observable.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -42,7 +42,7 @@ export class RecipeService {
     const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    this.http.post('https://recipebook-4a6f8.firebaseio.com/recipes.json', body, { headers: headers });
+    return this.http.post('https://recipebook-4a6f8.firebaseio.com/recipes.json', body, { headers: headers });
   }
 
   fetchData() {
