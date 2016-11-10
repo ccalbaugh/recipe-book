bdf79af7f99f3f7f45d6cea24a7c0784b8d8442e
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-10 14:51:32 -0600

    Cleaned Shopping-list related imports and declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,12 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 import { DropdownDirective } from './dropdown.directive';
 import { routing } from './app.routing';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start.component';
 import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -25,20 +24,17 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
     DropdownDirective,
     RecipeEditComponent,
     RecipeStartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     routing,
     ReactiveFormsModule
   ],
-  providers: [RecipeService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
