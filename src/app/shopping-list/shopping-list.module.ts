4b5aaf5781837110f0da50dbc32e624eb6cb0057
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-10 20:58:33 -0600

    Create and add shopping-list.routing to the module.

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingListAddComponent } from './shopping-list-add.component';
+import { shoppingListRouting } from './shopping-list.routing';
 
 @NgModule({
     declarations: [
     ShoppingListComponent,
     ShoppingListAddComponent
     ],
-    imports: [FormsModule, CommonModule] // CommonModule is used in leau of BrowserModule (that gives access to ngFor dir), which can only be used once.
+    imports: [FormsModule, CommonModule, shoppingListRouting] // CommonModule is used in leau of BrowserModule (that gives access to ngFor dir), which can only be used once.
 })
 export class ShoppingListModule {}
