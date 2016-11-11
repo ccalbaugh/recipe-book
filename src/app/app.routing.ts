9cfca86313034429c985b5a66a1257f5eb5b032f
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-10 20:57:47 -0600

    Change path to lazy load shopping-list.module.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,8 @@ const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'recipes', loadChildren: 'app/recipes/recipes.module#RecipesModule' }, // Because the WHOLE MODULE is being bootstrapped,
     // the path needs to be relative to src.  The class name of the MODULE ( the router file is imported to the module) is behind the #.
-    { path: 'shopping-list', component: ShoppingListComponent}
+
+    { path: 'shopping-list', loadChildren: 'app/shopping-list/shopping-list.module#ShoppingListModule' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
