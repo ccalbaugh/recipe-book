1591cbbb50a0860e7ce20036eb18d8252d36a05b
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-11 10:54:35 -0600

    Remove unnecessary import from routing file.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,5 @@
 import { Routes, RouterModule } from "@angular/router";
 
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HomeComponent } from './home.component';
 
 const APP_ROUTES: Routes = [
